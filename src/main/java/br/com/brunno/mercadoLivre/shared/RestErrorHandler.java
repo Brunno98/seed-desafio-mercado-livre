eb4a490519ab6250b0a1734370abe8eaae7b53c8
Brunno Santana <brunnosantana98@gmail.com>
2024-03-18 14:31:23 -0300

    retorno de erro no padrao RFC 7807

--- src/main/java/br/com/brunno/mercadoLivre/shared/RestErrorHandler.java
+++ src/main/java/br/com/brunno/mercadoLivre/shared/RestErrorHandler.java
@@ -1,17 +1,28 @@
 package br.com.brunno.mercadoLivre.shared;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestErrorHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public FieldErrorResponse handle(MethodArgumentNotValidException e) {
-        return new FieldErrorResponse(e.getFieldErrors());
+    public ProblemDetail handle(MethodArgumentNotValidException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "One or more validation errors occurred");
+        problemDetail.setProperty("errors", e.getFieldErrors().stream().map(fieldError -> {
+            String field = fieldError.getField();
+            String defaultMessage = Optional.ofNullable(fieldError.getDefaultMessage()).orElse("invalid");
+            return Map.of("field", field, "message", defaultMessage);
+        }).collect(Collectors.toList()));
+        return problemDetail;
     }
 }
